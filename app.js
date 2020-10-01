63ddb8a1c38d9c9313f55cca6fa5eed565efe517
KevinNMcpu <66437311+KevinNMcpu@users.noreply.github.com>
2020-10-01 00:59:10 -0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,32 @@
+var tbody = d3.select("tbody");
+
+d3.json("https://kevinnmcpu.github.io/samples.json").then(function(data) {
+    var list_of_names = data.names;
+    list_of_names.forEach(function(name){
+            var dropDown = d3.select("select");
+            var options = dropDown.append("option");
+            options.text(name).attr("id",name);
+        });
+  });
+
+d3.selectAll("#selDataset").on("change", updatePlotly);
+
+function updatePlotly() {
+    var dataset = d3.select('select').property('value');
+    //console.log(dataset);
+    //value of dataset selected^
+    
+    d3.json("https://kevinnmcpu.github.io/samples.json").then(function(data2) {
+            var selected_id = data2.metadata;
+            function search(dataset, selected_id){
+                for (var i=0; i < selected_id; i++) {
+                    if (selected_id[i].id === dataset){
+                        return selected_id[i];
+                        console.log(selected_id[i]);
+                    }
+                }
+            }
+            search();
+            //console.log(selected_id);
+        });
+}
