8d490fb4c09a77a5c26124b90cc206d0c0ba8d78
KevinNMcpu <66437311+KevinNMcpu@users.noreply.github.com>
2020-10-01 16:44:17 -0600

    Add files via upload

--- static/js/app.js
+++ static/js/app.js
@@ -13,19 +13,12 @@ d3.selectAll("#selDataset").on("change", updatePlotly);
 
 var more_data = d3.json("https://kevinnmcpu.github.io/samples.json");
 
-var biometric_data = more_data.metadata;
-
-function pull_meta_data(data)
-    {
-    var pulled_data = data.metadata;
-    return pulled_data;
-    }
+var metadata = more_data["metadata"];
 
 function updatePlotly() {
     var dataset = d3.select('select').property('value');
     //console.log(dataset);
     //value of dataset selected^
 
-    var rtr_data = pull_meta_data(more_data);
-    console.log(rtr_data);
+    console.log(metadata);
 };
