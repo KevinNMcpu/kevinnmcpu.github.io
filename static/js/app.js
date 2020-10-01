dfa93ccf5dac8a757324c13797255d6133a1c888
KevinNMcpu <66437311+KevinNMcpu@users.noreply.github.com>
2020-10-01 01:19:14 -0600

    Add files via upload

--- static/js/app.js
+++ static/js/app.js
@@ -13,17 +13,18 @@ d3.selectAll("#selDataset").on("change", updatePlotly);
 
 var more_data = d3.json("https://kevinnmcpu.github.io/samples.json");
 
-var metadata = more_data.metadata;
+var biometric_data = more_data.metadata;
 
+function search(nameKey, myArray){
+    for (var i=0; i < myArray.length; i++) {
+        if (myArray[i].name === nameKey) {
+            return myArray[i];
+        }
+    }
+}
 function updatePlotly() {
     var dataset = d3.select('select').property('value');
     console.log(dataset);
     //value of dataset selected^    
-
-                for (var i=0; i < metadata; i++) {
-                    console.log("running loop!")
-                    if (metadata[i].id === dataset){
-                        console.log(metadata[i]);
-                    }
-                }
+    console.log(search(dataset, biometric_data));
             };
