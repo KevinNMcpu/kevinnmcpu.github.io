b1fa836aded58fc96591bd5bbaaf32f51a79f0fe
KevinNMcpu <66437311+KevinNMcpu@users.noreply.github.com>
2020-10-01 00:31:04 -0600

    Add files via upload

--- static/js/app.js
+++ static/js/app.js
@@ -12,10 +12,11 @@ d3.json("https://kevinnmcpu.github.io/samples.json").then(function(data) {
 d3.selectAll("#selDataset").on("change", updatePlotly);
 
 function updatePlotly() {
-    var dropdownMenu = d3.select("#selDataset");
-    var dataset = dropdownMenu.property("id");
+    //var dropdownMenu = d3.select("#selDataset");
+    var dataset = d3.select('select').property('value');
+    //var dataset = dropdownMenu.property("id");
     //console.log("You picked " + dataset)
-    console.log(dropdownMenu);
+    console.log(dataset);
 
 
 
