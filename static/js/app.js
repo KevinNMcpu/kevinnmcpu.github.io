e5cd6694fc3951f6182fe08daf9da6f31606af49
KevinNMcpu <66437311+KevinNMcpu@users.noreply.github.com>
2020-09-30 22:33:12 -0600

    Add files via upload

--- static/js/app.js
+++ static/js/app.js
@@ -3,7 +3,7 @@ var tbody = d3.select("tbody");
 d3.json("https://kevinnmcpu.github.io/samples.json").then(function(data) {
     var list_of_names = data.names;
     list_of_names.forEach(function(name){
-            var dropDown = d3.select("tbody");
+            var dropDown = d3.select("tbody").append("select");
             var options = dropDown.select("select").data(name).enter().append("option").attr("id",name);
             options.text(name).attr("id",name);
         });
