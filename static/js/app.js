ed186ea6a454268a214ea70faf16d75152cc1889
KevinNMcpu <66437311+KevinNMcpu@users.noreply.github.com>
2020-09-30 18:11:29 -0600

    Add files via upload

--- static/js/app.js
+++ static/js/app.js
@@ -2,13 +2,8 @@ var list_of_names = []
 
 d3.json("https://kevinnmcpu.github.io/samples.json").then(function(data) {
     list_of_names = data.names;
-    console.log(list_of_names)
   });
 
 var tbody = d3.select("tbody");
 
-list_of_names.forEach(function(id)
-    {
-    var row = tbody.append("option").attr("value",id);
-    Object.entries(id).forEach(id)
-    })
+list_of_names.forEach(name => console.log(name));
